0e18ea8a47f0c53398cd5f90e8af4c3c023bae39
trinhchitai <tiemney@gmail.com>
2024-08-17 11:33:26 +0700

    Tui đang nghiên cứu làm

---
+++ TestAspWebApi/Core/DTO/DonHang/DonHangDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DTO.Product;
+using Core.Models;
+
+namespace Core.DTO.DonHang
+{
+    public class DonHangDTO
+    {
+        public int MaDonHang { get; set; }
+        public int SoLuong { get; set; }
+        public IEnumerable<ProductDTO> Products { get; set; }
+
+    }
+}
---
+++ TestAspWebApi/Core/Mapper/DonHangMapper.cs
@@ -0,0 +1,28 @@
+﻿using Core.DTO.Category;
+using Core.DTO.DonHang;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mapper
+{
+    public static class DonHangMapper
+    {
+        static DonHangDTO toDonHangDTO(this DonHang donhang)
+        {
+            return new DonHangDTO
+            {
+                MaDonHang = donhang.MaDonHang,
+                SoLuong = donhang.SoLuong,
+                Products = donhang.Product.Select(p => p.toProductDTO()).ToList()
+            };
+        }
+        static DonHang DonHangFromDTO(this DonHangDTO donhangDTO)
+        {
+            return new DonHang { MaDonHang = donhangDTO.MaDonHang };
+        }
+    }
+}
